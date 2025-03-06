930b1bc7faa8430b5bdd69e34c3aca048f5b5707
Andreas Neumann <83551558+JusTicE1986@users.noreply.github.com>
2025-03-06 12:09:19 +0100

    Filezugriff auf Lesezugriff geändert, sodass auch geöffnete Datei gelesen werden kann

--- ToolBox_Pro/Services/MerkmalCsvExportService.cs
+++ ToolBox_Pro/Services/MerkmalCsvExportService.cs
@@ -13,7 +13,9 @@ namespace ToolBox_Pro.Services
     {
         public void ExportiereMerkmale(string excelPfad, string ZielOrdner, string sheetName, string vcTyp)
         {
-            using var workbook = new XLWorkbook(excelPfad);
+            using (var stream = new FileStream(excelPfad, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            { 
+            using var workbook = new XLWorkbook(stream);
             var worksheet = workbook.Worksheet(sheetName);
 
             //Datenzeilen nach Kriterium filtern
@@ -38,104 +40,111 @@ namespace ToolBox_Pro.Services
 
             // CSV-Datei speichern, Merkmale in einer Zeile
             File.WriteAllText(csvFullPath, string.Join(";", kopfzeile), Encoding.UTF8);
+            }
         }
 
         public void ExportiereMerkmaleMitBezeichnung(string excelPfad, string zielOrdner, string sheetName)
         {
-            using var workbook = new XLWorkbook(excelPfad);
-            var worksheet = workbook.Worksheet(sheetName);
-
-            var merkmale = worksheet.RowsUsed().Skip(1)
-                .Where(row => row.Cell("J").GetString().Trim().Equals("JA", StringComparison.OrdinalIgnoreCase))
-                .Select(row => new
-                {
-                    Merkmal = row.Cell("C").GetString().Trim(),
-                    Bezeichnung = row.Cell("F").GetString().Trim(),
-                })
-                .Distinct()
-                .OrderBy(m => m.Merkmal)
-                .ToList();
-
-
-            // CSV-Dateiname erzeugen
-            string datumHeute = DateTime.Today.ToString("yyyy-MM-dd");
-            string csvFilename = $"{datumHeute}_Merkmale_mit_Bezeichnung.csv";
-            string csvFullPath = Path.Combine(zielOrdner, csvFilename);
-
-            // CSV schreiben
-
-            using var writer = new StreamWriter(csvFullPath, false, Encoding.UTF8);
-
-            // Kopfzeile
-            writer.WriteLine("Merkmal;Bezeichnung");
-
-            //Datenzeilen
-            foreach (var item in merkmale)
+            using (var stream = new FileStream(excelPfad, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
             {
-                string merkmal = item.Merkmal.Replace(";", ",");
-                string bezeichnung = item.Bezeichnung.Replace(";", ",");
-                writer.WriteLine($"{item.Merkmal};{item.Bezeichnung}");
+                using var workbook = new XLWorkbook(stream);
+                var worksheet = workbook.Worksheet(sheetName);
+
+                var merkmale = worksheet.RowsUsed().Skip(1)
+                    .Where(row => row.Cell("J").GetString().Trim().Equals("JA", StringComparison.OrdinalIgnoreCase))
+                    .Select(row => new
+                    {
+                        Merkmal = row.Cell("C").GetString().Trim(),
+                        Bezeichnung = row.Cell("F").GetString().Trim(),
+                    })
+                    .Distinct()
+                    .OrderBy(m => m.Merkmal)
+                    .ToList();
+
+
+                // CSV-Dateiname erzeugen
+                string datumHeute = DateTime.Today.ToString("yyyy-MM-dd");
+                string csvFilename = $"{datumHeute}_Merkmale_mit_Bezeichnung.csv";
+                string csvFullPath = Path.Combine(zielOrdner, csvFilename);
+
+                // CSV schreiben
+
+                using var writer = new StreamWriter(csvFullPath, false, Encoding.UTF8);
+
+                // Kopfzeile
+                writer.WriteLine("Merkmal;Bezeichnung");
+
+                //Datenzeilen
+                foreach (var item in merkmale)
+                {
+                    string merkmal = item.Merkmal.Replace(";", ",");
+                    string bezeichnung = item.Bezeichnung.Replace(";", ",");
+                    writer.WriteLine($"{item.Merkmal};{item.Bezeichnung}");
+                }
             }
         }
 
         public void ErstellePreislisteNachArtNr(string excelPfad, string zielOrdner, string sheetName, string vcTyp)
         {
-            using var workbook = new XLWorkbook(excelPfad);
-            var worksheet = workbook.Worksheet(sheetName);
+            using (var stream = new FileStream(excelPfad, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            {
+                using var workbook = new XLWorkbook(stream);
+                var worksheet = workbook.Worksheet(sheetName);
 
-            //Merkmale filtern und Kopfzeile vorbereiten
-            var merkmale = worksheet.RowsUsed().Skip(1)
-                .Where(row => row.Cell("J").GetString().Trim().Equals("JA") &&
-                                row.Cell("M").GetString().Trim().Equals(vcTyp, StringComparison.OrdinalIgnoreCase) &&
-                                row.Cell("C").GetString().Trim() != "Gesamtpreis")
-                .Select(row => row.Cell("C").GetString().Trim())
-                .Distinct()
-                .OrderBy(m => m)
-                .ToList();
+                //Merkmale filtern und Kopfzeile vorbereiten
+                var merkmale = worksheet.RowsUsed().Skip(1)
+                    .Where(row => row.Cell("J").GetString().Trim().Equals("JA") &&
+                                    row.Cell("M").GetString().Trim().Equals(vcTyp, StringComparison.OrdinalIgnoreCase) &&
+                                    row.Cell("C").GetString().Trim() != "Gesamtpreis")
+                    .Select(row => row.Cell("C").GetString().Trim())
+                    .Distinct()
+                    .OrderBy(m => m)
+                    .ToList();
 
 
-            merkmale.Insert(0, "Gesamtpreis");
+                merkmale.Insert(0, "Gesamtpreis");
 
-            // Gruppierung eindeutig nach Art-Nr.
-            var artikelGruppen = worksheet.RowsUsed().Skip(1)
-                .Where(row => row.Cell("J").GetString().Trim().Equals("JA") &&
-                row.Cell("M").GetString().Trim().Equals(vcTyp, StringComparison.OrdinalIgnoreCase))
-                .GroupBy(row => new
-                {
-                    ArtNr = row.Cell("A").GetString().Trim(),
-                    FzTyp = row.Cell("B").GetString().Trim()
-                });
+                // Gruppierung eindeutig nach Art-Nr.
+                var artikelGruppen = worksheet.RowsUsed().Skip(1)
+                    .Where(row => row.Cell("J").GetString().Trim().Equals("JA") &&
+                    row.Cell("M").GetString().Trim().Equals(vcTyp, StringComparison.OrdinalIgnoreCase))
+                    .GroupBy(row => new
+                    {
+                        ArtNr = row.Cell("A").GetString().Trim(),
+                        FzTyp = row.Cell("B").GetString().Trim()
+                    });
                 //.ToList();
 
-            // csv erstellen
-            string datumHeute = DateTime.Today.ToString("yyyy-MM-dd");
-            string csvFileName = $"{datumHeute}_Preisliste_{vcTyp.Replace(" ", "_")}.csv";
-            string csvFullPath = Path.Combine(zielOrdner, csvFileName);
+                // csv erstellen
+                string datumHeute = DateTime.Today.ToString("yyyy-MM-dd");
+                string csvFileName = $"{datumHeute}_Preisliste_{vcTyp.Replace(" ", "_")}.csv";
+                string csvFullPath = Path.Combine(zielOrdner, csvFileName);
 
-            using var writer = new StreamWriter(csvFullPath, false, Encoding.UTF8);
+                using var writer = new StreamWriter(csvFullPath, false, Encoding.UTF8);
 
-            // Header schreiben
-            var header = new List<string> { "Art-Nr", "Typ" };
-            header.AddRange(merkmale);
-            writer.WriteLine(string.Join(";", header));
+                // Header schreiben
+                var header = new List<string> { "Art-Nr", "Typ" };
+                header.AddRange(merkmale);
+                writer.WriteLine(string.Join(";", header));
 
-            //Datenzeilen schreiben (eine pro Art-Nr.
-            foreach (var gruppe in artikelGruppen)
-            {
-                var zeile = new List<string>
+                //Datenzeilen schreiben (eine pro Art-Nr.
+                foreach (var gruppe in artikelGruppen)
+                {
+                    var zeile = new List<string>
                 {
                     gruppe.Key.ArtNr,
                     gruppe.Key.FzTyp
                 };
 
-                foreach (var merkmal in merkmale)
-                {
-                    var preisZelle = gruppe.FirstOrDefault(row => row.Cell("C").GetString().Trim() == merkmal);
-                    string preis = preisZelle != null ? preisZelle.Cell("I").GetString().Trim() : "";
+                    foreach (var merkmal in merkmale)
+                    {
+                        var preisZelle = gruppe.FirstOrDefault(row => row.Cell("C").GetString().Trim() == merkmal);
+                        string preis = preisZelle != null ? preisZelle.Cell("I").GetString().Trim() : "";
 
-                    zeile.Add(preis);
+                        zeile.Add(preis);
+                    }
+                    writer.WriteLine(string.Join(";", zeile));
                 }
-                writer.WriteLine(string.Join(";", zeile));
             }
         }
     }
